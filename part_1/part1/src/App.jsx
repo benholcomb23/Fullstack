eea7c8a9d92b7190fbf7f838ddf05c608ad7741c
Ben Holcomb <benholcomb5@gmail.com>
2026-01-30 21:44:14 -0600

    ok... think everything is up to date now

--- part_1/part1/src/App.jsx
+++ part_1/part1/src/App.jsx
@@ -3,13 +3,23 @@ import { useState } from 'react'
 const App = () => {
   const [ counter, setCounter ]= useState(0)
 
-  setTimeout(
-    () => setCounter(counter + 1),
-    1000
-  )
+  const Display = ({counter}) => <div>{counter}</div>
+  
+
+  const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>
+
+  const increaseByOne = () => setCounter(counter + 1)
+  const decreaseByOne = () => setCounter(counter - 1)
+  const setToZero = () => setCounter(0)
 
   return  (
-    <div>{counter}</div>
+    <div>
+
+    <Display counter = {counter}/>
+    <Button onClick = {increaseByOne} text = "plus"/>
+    <Button onClick = {decreaseByOne} text = "minus"/>
+    <Button onClick = {setToZero} text = "Reset"/>
+    </div>
   )
 }
 
