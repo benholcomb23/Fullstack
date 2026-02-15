66d4fea9c3fb03c7f787abf65844221a67c0a6f0
Ben Holcomb <benholcomb5@gmail.com>
2026-02-15 00:12:29 -0600

    finished all part 2-b exercises with the phonebook

--- part_2/the_phonebook/src/App.jsx
+++ part_2/the_phonebook/src/App.jsx
@@ -1,28 +1,87 @@
 import { useState } from 'react'
 
-const Numbers = (props) => {
+const People = (props) => {
   return (
     <>
-    {props.name} 
+    {props.name} {props.number}
     </>
   )
 }
-const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas',
-      id: 1
-     }
-  ]) 
-  const [newName, setNewName] = useState('')
 
+const Numbers = (props) => {
+  return (
+    <>
+      <h2>Numbers</h2>
+      <ul>
+        {props.filtered ? props.filteredPersons.map((people) => (
+          <li key={people.id}><People name={people.name} number={people.phone_number}/></li>
+        )) 
+        : props.persons.map((people) => (
+          <li key={people.id}><People name={people.name} number={people.phone_number}/></li>
+        )) 
+        }
+      </ul>
+    </>
+  )
+}
+
+const Filter = (props) => {
+  return (
+    <form>
+    <div>
+    filter by name: <input value={props.filterText} onChange={props.handleFilterText}/>
+    </div>
+    <div>
+      <button 
+      type="submit"
+      onClick={props.setFilter}
+      >
+      filter
+      </button>
+    </div>
+  </form>
+  )
+}
+
+const NewPersonForm = (props) => {
+  return(
+  <>
+  <h2>add a new</h2>
+  <form>
+  <div>
+    name: <input value ={props.newName} onChange={props.handleAddPerson}/>   
+  </div>
+  <div>
+    phone #: <input value={props.newPhoneNumber} onChange={props.handleNewPhone}/>
+  </div>
+  <div>
+    <button 
+    type="submit" 
+    onClick={props.addPerson}
+    >
+    add
+    </button>
+  </div>
+  </form>
+  </>
+  )
+}
+
+
+const App = (props) => {
+  const [persons, setPersons] = useState(props.persons) 
+  const [newName, setNewName] = useState('')
+  const [newPhoneNumber, setNewPhoneNumber]= useState('')
+  const [filterText, setFilterText] = useState('')
+  const [filteredPersons, setfilteredPersons] = useState(persons)
+  const [isFiltered, setIsFiltered] = useState(false)
   
   const addPerson = (event) => {
     const existingPerson = persons.find(person => person.name === newName)
-    console.log(existingPerson)
     event.preventDefault()
-    console.log(newName)
     const person = {
         name: newName,
+        phone_number: newPhoneNumber,
         id: String(persons.length + 1)
       }
     if (existingPerson) {
@@ -40,34 +99,31 @@ const App = () => {
     }
   }
 
+  const setFilter = (event) => {
+    event.preventDefault()
+    setfilteredPersons(persons.filter(person => person.name.toLowerCase().startsWith(filterText.toLowerCase())))
+    setIsFiltered(true)
+    console.log(filteredPersons)
+  }
+
   const handleAddPerson = (event) => {
     setNewName(event.target.value)
-    console.log(event.target.value)
+  }
+  
+  const handleNewPhone = (event) => {
+    setNewPhoneNumber(event.target.value)
+  }
+
+  const handleFilterText = (event) => {
+    setFilterText(event.target.value)
   }
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
-        <div>
-          name: <input value ={newName} onChange={handleAddPerson}/>
-        </div>
-        <div>
-          <button 
-          type="submit" 
-          onClick={addPerson}
-          >
-          add
-          </button>
-        </div>
-      </form>
-      <h2>Numbers</h2>
-      <ul>
-        {persons.map((number) => (
-          <li><Numbers name={number.name} /></li>
-        ))}
-      </ul>
-      
+      <Filter filterText={filterText} handleFilterText={handleFilterText} setFilter={setFilter}/>
+      <NewPersonForm newName={newName} handleAddPerson={handleAddPerson} newPhoneNumber={newPhoneNumber} handleNewPhone={handleNewPhone} addPerson={addPerson}/>
+      <Numbers filtered={isFiltered} filteredPersons={filteredPersons} persons={persons}/>
     </div>
   )
 }
--- part_2/the_phonebook/src/main.jsx
+++ part_2/the_phonebook/src/main.jsx
@@ -1,4 +1,19 @@
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(<App />)
+const persons = [
+    { name: 'Arto Hellas',
+      number: 365,
+      id: 1
+     }, 
+     { name: 'Ben',
+      number: 254,
+      id: 2
+     },
+     { name: 'Hannah',
+      number: 785,
+      id: 3
+     }
+    ]
+
+createRoot(document.getElementById('root')).render(<App persons={persons}/>)
