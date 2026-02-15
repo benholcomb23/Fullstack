561a3bafd3b17ef7af2ca5dde30ece2ec4195c2d
benholcomb23 <ben.holcomb5@gmail.com>
2026-02-14 22:50:55 -0600

    worked on exercises 2.6 and 2.7. Now time for part 3 of the phonebook...!

--- part_2/the_phonebook/src/App.jsx
+++ part_2/the_phonebook/src/App.jsx
@@ -1,24 +1,73 @@
 import { useState } from 'react'
 
+const Numbers = (props) => {
+  return (
+    <>
+    {props.name} 
+    </>
+  )
+}
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas',
+      id: 1
+     }
   ]) 
   const [newName, setNewName] = useState('')
+
+  
+  const addPerson = (event) => {
+    const existingPerson = persons.find(person => person.name === newName)
+    console.log(existingPerson)
+    event.preventDefault()
+    console.log(newName)
+    const person = {
+        name: newName,
+        id: String(persons.length + 1)
+      }
+    if (existingPerson) {
+      console.log('err')
+      alert('hey this thing exists')
+    }
+    else if (newName === '') {
+      console.log('err')
+      alert('hey you gotta put something here')
+    }
+    else {
+    setPersons(persons.concat(person))
+        
+    setNewName('')
+    }
+  }
+
+  const handleAddPerson = (event) => {
+    setNewName(event.target.value)
+    console.log(event.target.value)
+  }
 
   return (
     <div>
       <h2>Phonebook</h2>
       <form>
         <div>
-          name: <input />
+          name: <input value ={newName} onChange={handleAddPerson}/>
         </div>
         <div>
-          <button type="submit">add</button>
+          <button 
+          type="submit" 
+          onClick={addPerson}
+          >
+          add
+          </button>
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+      <ul>
+        {persons.map((number) => (
+          <li><Numbers name={number.name} /></li>
+        ))}
+      </ul>
+      
     </div>
   )
 }
