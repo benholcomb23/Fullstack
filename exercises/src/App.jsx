48bb3f1bd2ece50410b28d7837e791f1a0485f9d
benholcomb23 <ben.holcomb5@gmail.com>
2026-02-24 21:27:25 -0600

    completed exercises related to inline css in React

--- exercises/src/App.jsx
+++ exercises/src/App.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react'
 import Note from './components/Note'
+import Footer from './components/Footer'
 import noteService from './services/notes'
 import './index.css'
 import Notification from './components/notification'
@@ -94,6 +95,7 @@ const App = () => {
         />
         <button type='submit'>save</button>
       </form>
+      <Footer />
     </div>
   )
 }
